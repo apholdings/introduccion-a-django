608fb023d93772923893dddbe9e944687686a9f1
NineRogues <alexander.eric.3@gmail.com>
2021-08-12 16:13:46 -0500

    CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView, BlogCreateView, BlogDetailView, BlogUpdateView, BlogDeleteView
 
 app_name="blog"
 
 urlpatterns = [
     path('', BlogListView.as_view(), name="home"),
+    path('create/', BlogCreateView.as_view(), name="create"),
+    path('<int:pk>/', BlogDetailView.as_view(), name="detail"),
+    path('<int:pk>/update/', BlogUpdateView.as_view(), name="update"),
+    path('<int:pk>/delete/', BlogDeleteView.as_view(), name="delete")
+
 ]
